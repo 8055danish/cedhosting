0af03093fb677e610b55fa4de63fecb70f29a047
8055danish <danishmohammad597@gmail.com>
2020-12-08 10:26:14 +0530

    08Dec20 10:26

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php require "header.php"; ?>
+	<!---banner--->
+		<div class="banner">
+			<div class="container">
+				<div class="banner-grids">
+					<div class="col-md-6 banner-grid">
+						<img src="images/srceen.png" class="img-responsive" alt=""/>
+					</div>
+					<div class="col-md-6 banner-grid">
+						<h3>Unlimited Web Hosting</h3>
+						<p>this hero area to show off some of your nice work. You can even have a video inside it since it’s great as a secondary call to action alongside this button underneath this text.</p>
+						<a href="single.html" class="button">get started</a>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+	<!---banner--->
+	<!---brilliantly --->
+			<div class="content">
+				<div class="brilliant-section">
+					<div class="container">
+						<h2>this is what we do.</h2>
+						<h5>We get the job done, no matter the project</h5>
+						<div class="brilliant-grids">
+							<div class="col-md-4 brilliant-grid">
+								<div class="brilliant-left">
+								<i class="glyphicon glyphicon-cog" aria-hidden="true"></i>
+								</div>
+								<div class="brilliant-right">
+									<h4>Expert Web Design</h4>
+									<p>We strive to deliver the very best possible work that’s available out there, at any time. That’s how we set ourselves apart.</p>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="col-md-4 brilliant-grid">
+								<div class="brilliant-left">
+								<i class="glyphicon glyphicon-cloud" aria-hidden="true"></i>
+								</div>
+								<div class="brilliant-right">
+									<h4>ftp services</h4>
+									<p>We strive to deliver the very best possible work that’s available out there, at any time. That’s how we set ourselves apart.</p>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="col-md-4 brilliant-grid">
+								<div class="brilliant-left">
+								<i class="glyphicon glyphicon-signal" aria-hidden="true"></i>
+								</div>
+								<div class="brilliant-right">
+									<h4>Support Service</h4>
+									<p>We strive to deliver the very best possible work that’s available out there, at any time. That’s how we set ourselves apart.</p>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<div class="brilliant-grids">
+							<div class="col-md-4 brilliant-grid">
+								<div class="brilliant-left">
+								<i class="glyphicon glyphicon-globe" aria-hidden="true"></i>
+								</div>
+								<div class="brilliant-right">
+									<h4>multi domain</h4>
+									<p>We strive to deliver the very best possible work that’s available out there, at any time. That’s how we set ourselves apart.</p>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="col-md-4 brilliant-grid">
+								<div class="brilliant-left">
+								<i class="glyphicon glyphicon-link" aria-hidden="true"></i>
+								</div>
+								<div class="brilliant-right">
+									<h4>Link Building</h4>
+									<p>We strive to deliver the very best possible work that’s available out there, at any time. That’s how we set ourselves apart.</p>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="col-md-4 brilliant-grid">
+								<div class="brilliant-left">
+								<i class="glyphicon glyphicon-phone" aria-hidden="true"></i>
+								</div>
+								<div class="brilliant-right">
+									<h4>Mobile Optimization</h4>
+									<p>We strive to deliver the very best possible work that’s available out there, at any time. That’s how we set ourselves apart.</p>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+					</div>
+				</div>
+			<!---brilliantly--->
+			<?php require "footer.php"; ?>
